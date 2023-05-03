f1efd03e11000d6c76df6ee633d35aadb512b253
Var-01 <vartikaverma011929@gmail.com>
2023-05-03 23:44:47 +0530

    progressing with the code

--- recommend.py
+++ recommend.py
@@ -10,3 +10,22 @@ class LaptopRecommendationSystem:
     
     def __init__(self, path_to_csv_file):
         self.df = pd.read_csv(path_to_csv_file)
+
+    def filter_data(self, brand, color=None, battery_life=None, hard_disk_size=None, max_price=None):
+        filtered_data = self.df[self.df['Brand'] == brand]  
+
+        if color:
+            filtered_data = filtered_data[filtered_data['Colours'] == color] 
+
+        if battery_life:
+            filtered_data = filtered_data[filtered_data['Battery_Life'] == battery_life]  
+
+        if hard_disk_size:
+            filtered_data = filtered_data[filtered_data['Hard disk'] == hard_disk_size]  
+
+        if max_price:
+            filtered_data['Price in India'] = filtered_data['Price in India'].apply(
+                lambda x: float(str(x).replace(",", "")))
+            filtered_data = filtered_data[filtered_data['Price in India'] <= max_price]
+
+        return filtered_data
